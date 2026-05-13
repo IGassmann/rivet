331c4c5902de4fa1850ecdfb5fd258871184d6ca
Igor Gassmann <igor@igassmann.me>
2026-05-13 21:02:02 +0200

    refactor(effect): remove `ActorName`

--- rivetkit-typescript/packages/effect/src/ActorName.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "effect";
-
-export const ActorName = Schema.String.pipe(
-	Schema.brand("~@rivetkit/effect/ActorName"),
-);
-
-export type ActorName = typeof ActorName.Type;
-
-export const make = (value: string): ActorName => value as ActorName;
